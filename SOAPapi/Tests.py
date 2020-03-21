1f89aa68a8d1037fe6f8edf579ed5ee4f90c038d
altazur <altazur.91@gmail.com>
2020-03-22 01:22:35 +0200

    Assert result to string

--- SOAPapi/Tests.py
+++ SOAPapi/Tests.py
@@ -1,4 +1,4 @@
 import pytest
 
 def test_soap_calc_add(get_calculator, soap_version):
-   assert get_calculator.add(1,5, soap_version) == 6
+   assert get_calculator.add(1,5, soap_version) == str(6)
