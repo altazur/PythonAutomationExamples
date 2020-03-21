86412fb2cba7d36374a9cc9ae8ed2c1c19700662
altazur <altazur.91@gmail.com>
2020-03-22 00:57:12 +0200

    Add soap version option

--- SOAPapi/conftest.py
+++ SOAPapi/conftest.py
@@ -5,3 +5,10 @@ import pytest
 def get_calculator():
     calculator = Calculator()
     yield calculator
+
+def pytest_addoption(parser):
+    parser.addoption('--SOAPver', default='1', help='SOAP version: 1 or 2')
+
+@pytest.fixture
+def soap_version(request):
+    return request.config.getoption('--SOAPver')
