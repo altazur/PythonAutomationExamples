a9eb901bdda89338466972a1549e544ef7f93f58
altazur <altazur.91@gmail.com>
2020-03-22 00:58:20 +0200

    Add soap version fixture to test

--- SOAPapi/Tests.py
+++ SOAPapi/Tests.py
@@ -1,4 +1,4 @@
 import pytest
 
-def test_soap_calc_add(get_calculator):
-   assert get_calculator.add(1,5) == 6
+def test_soap_calc_add(get_calculator, soap_version):
+   assert get_calculator.add(1,5, soap_version) == 6
